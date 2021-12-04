977b1c35c6967c2918bf41fa9322d7199eb7e9ce
Skorodinskaya <skorodinskaya01@gmail.com>
2021-12-05 01:17:10 +0200

    homework 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
-  {
-    path: '', component: HomeComponent, children: [
+  {path: '', component: HomeComponent, children: [
       {path: '', redirectTo: 'login', pathMatch: 'full'},
       {path: 'login', component: LoginComponent},
       {path: 'admin', loadChildren:() => import('./components/admin/admin.module').then(value => value.AdminModule)}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import {MainInterceptor} from "./main.interceptor";
 
 @NgModule({
   declarations: [
@@ -22,7 +23,11 @@ import { LoginComponent } from './components/login/login.component';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    multi: true,
+    useClass: MainInterceptor
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
